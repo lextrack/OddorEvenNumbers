9e55b2db3cd2541279b806391567308654d3e9db
lextrack <arielmunozg97@gmail.com>
2023-01-13 00:02:54 -0300

    Add project files.

---
+++ OddorEven/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OddorEven
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnIngreso_Click(object sender, RoutedEventArgs e)
+        {
+            Operaciones ObjetoEvaluar = new Operaciones();
+            try
+            {
+                BtnIngreso.ContentStringFormat = Convert.ToString(ObjetoEvaluar.Primos(Convert.ToInt64(numero.Text)));
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("You can't enter letters, symbols or left the TextBox blank.", "Error");
+            }
+
+        }
+
+        private void numero_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ OddorEven/Operaciones.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OddorEven
+{
+    public class Operaciones
+    {
+        public bool Primos(long num)
+        {
+            if (num % 2 == 0)
+            {
+                MessageBox.Show("This number is even", "Message");
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("This number is odd", "Message");
+                return false;
+            }
+        }
+    }
+}
+
